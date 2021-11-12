e35c8447e882800791b8c30e6c56d9f93fbaa7bd
JohnBensen1000 <jbensen@purdue.edu>
2021-11-11 20:47:15 -0500

    no longer gets github url from zipped package bc apparantly we don't have to do that

--- project2/registry/functions.py
+++ project2/registry/functions.py
@@ -26,58 +26,6 @@ def get_file_content(filePath):
     with open(filePath, "rb") as file:
         return file.read().decode("Cp437")
 
-# This function searches through a zipped packages "package.json" file to look for a github
-# url. This url can be stored in multiple ways, include:
-#   "repository": "[repository name]"
-#   "homepage":   "https://github.com/[repository name]"
-#   "repository": {
-#       "type": "git"
-#       "url":  "https://github.com/[repository name]"
-#   }
-# It is also possible that there is not github url in the package.json file. The way in which
-# the url is stored is not known in advance, so all of these possibilities have to be checked
-# for. 
-
-def get_github_url_from_zipped_package(file):
-    with zipfile.ZipFile(file) as zipped:
-        packageJsonName = ""
-        for name in zipped.namelist():
-            if "package.json" in name:
-                packageJsonName = name
-                break
-
-        mainLine           = b""
-        inRepositoryObject = False
-        with zipped.open(packageJsonName) as file:
-            for line in file.readlines():
-                if b"homepage" in line and b"github.com" in line:
-                    mainLine = line
-                    break
-
-                elif b"repository" in line and b"{" not in line:
-                    mainLine = line
-                    break
-
-                elif b"repository" in line:
-                    inRepositoryObject = True
-
-                elif inRepositoryObject and b"url" in line:
-                    mainLine = line
-                    break
-
-    mainLineString = mainLine.decode("utf-8")
-
-    if "github.com" not in mainLineString and "repository" in mainLineString:
-        mainLineString = mainLineString.split("\"")[3]
-        mainLineString = "https://github.com/" + mainLineString
-
-    if "\"homepage\"" in mainLineString or "\"url\":" in mainLineString:
-        mainLineString = mainLineString.split("\"")[3]
-
-    githubUrl = mainLineString.split(".git")[0]
-
-    return githubUrl
-
 # Given a github url for a npm package, uses the functionality provided by project 1 to
 # calculate and return the package's overall score and a list of its subscores. 
 
