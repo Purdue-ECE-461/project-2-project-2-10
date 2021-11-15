93451efd5e74d9e761d57b3b2410a6e36fde7601
ric-rhee <rheer@purdue.edu>
2021-11-14 22:01:08 -0500

    html file for user registration

--- project2/registry/project1/readEnvironment.py
+++ project2/registry/project1/readEnvironment.py
@@ -1,11 +1,11 @@
 import os
 
-# GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
-# LOG_LEVEL = os.getenv('LOG_LEVEL')
-# LOG_FILE = os.getenv('LOG_FILE')
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+LOG_LEVEL = os.getenv('LOG_LEVEL')
+LOG_FILE = os.getenv('LOG_FILE')
 
 
-GITHUB_TOKEN = "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"
-LOG_LEVEL = "0"
-LOG_FILE = "/Users/johnbensen/Documents/ECE/ECE461/project-2-project-2-10/log"
+#GITHUB_TOKEN = "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"
+#LOG_LEVEL = "0"
+#LOG_FILE = "/Users/johnbensen/Documents/ECE/ECE461/project-2-project-2-10/log"
 
